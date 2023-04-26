997f200499f3f412d1802e52e9391e3c86e18929
lucywohltorf <lucywohltorf@air-von-lucy.localdomain>
2023-04-26 14:24:26 +0200

    feat: add a Password-task

--- password-toogle/script.js
+++ password-toogle/script.js
@@ -1,6 +1,6 @@
 function togglePasswordVisibility() { // Eine Funktion namens "togglePasswordVisibility" wird definiert.
-  var passwordInput = document.getElementById("password-input"); // Das HTML-Element mit der ID "password-input" wird der Variablen "passwordInput" zugewiesen.
-  var toggleButton = document.getElementById("toggle-button"); // Das HTML-Element mit der ID "toggle-button" wird der Variablen "toggleButton" zugewiesen.
+  let  passwordInput = document.getElementById("password-input"); // Das HTML-Element mit der ID "password-input" wird der Variablen "passwordInput" zugewiesen.
+  let toggleButton = document.getElementById("toggle-button"); // Das HTML-Element mit der ID "toggle-button" wird der Variablen "toggleButton" zugewiesen.
   if (passwordInput.type === "password") { // Wenn das Eingabefeld für das Passwort vom Typ "password" ist, führe die folgenden Schritte aus:
     passwordInput.type = "text"; // Ändere den Typ des Eingabefelds für das Passwort zu "text".
     toggleButton.textContent = "Hide Password"; // Ändere den Text des "toggle-button" zu "Hide Password".
